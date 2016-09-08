b679973a7f80620787d17796fcb680fe1471cc6b
Sven Nilsen <bvssvni@gmail.com>
2016-09-08 12:02:14 +0200

    Fixed spelling error

--- src/construct.rs
+++ src/construct.rs
@@ -1,6 +1,6 @@
 /// Constructs a new space.
 ///
-/// Since spaces are combines as phantom types,
+/// Since spaces are combined using phantom types,
 /// an instance must be constructed to call methods on it.
 pub trait Construct {
     /// Constructs a new Self.
